21c359150a76b43019d280fdb803f1047e52487b
FedericoMosso <federicomosso762@gmail.com>
2023-06-21 20:01:06 -0300

    ultimo

--- public/conection.js
+++ public/conection.js
@@ -5,18 +5,13 @@ function emit_data() {
     socket.emit( //emit envia datos (en este caso desde el cliente hacia el servidor)
         'primer_conexion', //identificador del mensaje
         { //objeto con las propiedades a enviar (en este caso al servidor)
-            name: 'Federico',
-            last_name: 'Mosso',
-            age: 25
+            name: 'Nico',
+            last_name: 'Lopez',
+            age: 37
         }
     )
 }
-/*
-socket.on('connect', () => {
-    let selector = document.querySelectorAll('#btn')
-    selector.forEach(each => each.addEventListener('click'))
-    socket.emit('agregar_a_carrito')
-})*/
+
 
 socket.on("cartUpdated", (cartContent) => {
     console.log("el carrito tiene:", cartContent, "contenidos")
@@ -25,11 +20,18 @@ socket.on("cartUpdated", (cartContent) => {
     contadorSpan.innerText = cartContent
 })
 
-socket.emit("getCartContent", currentCart)
+socket.on("userCartId", (cartId) => {
+    console.log("el carrito tiene:", cartContent, "contenidos")
+    sessionStorage.setItem("userCart", cartId)
+})
 
-/*
-socket.on('num_products', numContador => {
-    console.log("recibido")
-    const contadorSpan = document.getElementById('contador');
-    contadorSpan.innerText = numContador
-}) */
+socket.emit("getCartContent", sessionStorage.getItem("userCart"))
+if (sessionStorage.getItem("userCart") == undefined) {
+    const req = fetch("http://localhost:3000/api/carts", {
+        method: "POST"
+    })
+    .then(res => res.json())
+    .then(response => {
+        sessionStorage.setItem("userCart", response.id)
+    })
+} // le envia al servidor una solicitud para que haga un carrito para el usuario
